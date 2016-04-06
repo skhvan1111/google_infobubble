132ded7e24a4120769130c98213c7835b23d327e
skhvan1111 <skhvan1111@gmail.com>
2016-04-06 15:46:29 +0000

    offset added

--- infobubble.js
+++ infobubble.js
@@ -846,6 +846,26 @@ InfoBubble.prototype.onAdd = function() {
 InfoBubble.prototype['onAdd'] = InfoBubble.prototype.onAdd;
 
 
+/*
+* Sets InfoBubble Position
+* */
+InfoBubble.prototype.setBubbleOffset=function(xOffset,yOffset)
+{
+    this.bubbleOffsetX=parseInt(xOffset);
+    this.bubbleOffsetY=parseInt(yOffset);
+}
+
+/*
+ * Gets InfoBubble Position
+ * */
+InfoBubble.prototype.getBubbleOffset=function()
+{
+    return {
+        x:this.bubbleOffsetX||0,
+        y:this.bubbleOffsetY||0
+    }
+}
+
 /**
  * Draw the InfoBubble
  * Implementing the OverlayView interface
@@ -854,76 +874,76 @@ InfoBubble.prototype.draw = function() {
   var projection = this.getProjection();
 
   if (!projection) {
-    // The map projection is not ready yet so do nothing
-    return;
+      // The map projection is not ready yet so do nothing
+      return;
   }
-
+  
   var latLng = /** @type {google.maps.LatLng} */ (this.get('position'));
-
+  
   if (!latLng) {
-    this.close();
-    return;
+      this.close();
+      return;
   }
-
+  
   var tabHeight = 0;
-
+  
   if (this.activeTab_) {
-    tabHeight = this.activeTab_.offsetHeight;
+      tabHeight = this.activeTab_.offsetHeight;
   }
-
+  
   var anchorHeight = this.getAnchorHeight_();
   var arrowSize = this.getArrowSize_();
   var arrowPosition = this.getArrowPosition_();
-
+  
   arrowPosition = arrowPosition / 100;
-
+  
   var pos = projection.fromLatLngToDivPixel(latLng);
   var width = this.contentContainer_.offsetWidth;
   var height = this.bubble_.offsetHeight;
-
+  
   if (!width) {
-    return;
+      return;
   }
-
+  
   // Adjust for the height of the info bubble
-  var top = pos.y - (height + arrowSize);
-
+  var top = pos.y - (height + arrowSize)+this.getBubbleOffset().y;
+  
   if (anchorHeight) {
-    // If there is an anchor then include the height
-    top -= anchorHeight;
+      // If there is an anchor then include the height
+      top -= anchorHeight;
   }
-
-  var left = pos.x - (width * arrowPosition);
-
+  
+  var left = pos.x - (width * arrowPosition)+this.getBubbleOffset().x;
+  
   this.bubble_.style['top'] = this.px(top);
   this.bubble_.style['left'] = this.px(left);
-
+  
   var shadowStyle = parseInt(this.get('shadowStyle'), 10);
-
+  
   switch (shadowStyle) {
-    case 1:
-      // Shadow is behind
-      this.bubbleShadow_.style['top'] = this.px(top + tabHeight - 1);
-      this.bubbleShadow_.style['left'] = this.px(left);
-      this.bubbleShadow_.style['width'] = this.px(width);
-      this.bubbleShadow_.style['height'] =
-          this.px(this.contentContainer_.offsetHeight - arrowSize);
-      break;
-    case 2:
-      // Shadow is below
-      width = width * 0.8;
-      if (anchorHeight) {
-        this.bubbleShadow_.style['top'] = this.px(pos.y);
-      } else {
-        this.bubbleShadow_.style['top'] = this.px(pos.y + arrowSize);
-      }
-      this.bubbleShadow_.style['left'] = this.px(pos.x - width * arrowPosition);
-
-      this.bubbleShadow_.style['width'] = this.px(width);
-      this.bubbleShadow_.style['height'] = this.px(2);
-      break;
+      case 1:
+          // Shadow is behind
+          this.bubbleShadow_.style['top'] = this.px(top + tabHeight - 1);
+          this.bubbleShadow_.style['left'] = this.px(left);
+          this.bubbleShadow_.style['width'] = this.px(width);
+          this.bubbleShadow_.style['height'] =
+              this.px(this.contentContainer_.offsetHeight - arrowSize);
+          break;
+      case 2:
+          // Shadow is below
+          width = width * 0.8;
+          if (anchorHeight) {
+              this.bubbleShadow_.style['top'] = this.px(pos.y);
+          } else {
+              this.bubbleShadow_.style['top'] = this.px(pos.y + arrowSize);
+          }
+          this.bubbleShadow_.style['left'] = this.px(pos.x - width * arrowPosition);
+  
+          this.bubbleShadow_.style['width'] = this.px(width);
+          this.bubbleShadow_.style['height'] = this.px(2);
+          break;
   }
-};
+}
 InfoBubble.prototype['draw'] = InfoBubble.prototype.draw;
 
 
